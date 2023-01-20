3ae27b1318767344572f81498a0ed4708ce21d37
Rick Herodes <51223848+rherodes@users.noreply.github.com>
2023-01-20 13:38:15 -0500

    Create OptionsGUIPanel.java

---
+++ OptionsGUIPanel.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+
+public class OptionsGUIPanel extends JPanel
+{
+   private final int WIDTH = 1900, HEIGHT = 950;
+
+   public BlackjackPanel()
+	 {
+      
+   }
+   
+} // end panel
